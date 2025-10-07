ebc0bdd07df9d439136e9435dfc8c42dc6003928
Kilian Barantal <kili@nbarantal.me>
2025-10-07 11:39:23 +0200

    remove unused calibration and point constants from settings.py

--- utils/settings.py
+++ utils/settings.py
@@ -1,12 +1,7 @@
 # settings.py
 import os
 
-CALIBRATION_FILE = os.path.join(os.path.dirname(__file__),"imu_calibration.npz")
-GYRO_CALIBRATION_FILE = os.path.join(os.path.dirname(__file__),"gyro_calibration.npz")
 DT = 0.05
-POINT_BOUEE = (48.20010, -3.01573)
-POINT_BASE = (48.1996872, -3.0153766)
 RHO = 6371000
-FREQUENCE_CIRCLE = 1/120
-RAYON_CIRCLE = 20
+
 
